f166add696ee66188fa49aee6c3e032ae65940b9
rainbowcrack <145824500+rainbowcrack@users.noreply.github.com>
2023-10-06 23:24:01 -0300

    Update EarthDataL2aEmit.py

--- EarthDataL2aEmit.py
+++ EarthDataL2aEmit.py
@@ -4,9 +4,21 @@
 # intitle:index.of + "emit" + "nasa" or "jpl" 
 # rebember to use a VPN, because there are crawlers!!
 
-pip install netCDF4
+pip install netCDF4 as nc
+pip install pandas as pd
+pip install os 
+pip install osgeo 
+from spectral.io import envi
+from osgeo import gdal
 import netCDF4 as nc
 import csv
+import numpy as np
+import math
+import pandas as pd
+import xarray as xr
+import rasterio as rio
+import s3fs
+from fsspec.implementations.http import HTTPFile
 
 # https://data.lpdaac.earthdatacloud.nasa.gov/lp-prod-protected/EMITL2ARFL.001/EMIT_L2A_RFL_001_20220903T163129_2224611_012/EMIT_L2A_RFL_001_20220903T163129_2224611_012.nc
 # https://data.lpdaac.earthdatacloud.nasa.gov/lp-prod-protected/EMITL2ARFL.001/EMIT_L2A_RFL_001_20220903T163129_2224611_012/EMIT_L2A_MASK_001_20220903T163129_2224611_012.nc
